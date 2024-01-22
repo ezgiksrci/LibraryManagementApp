daef3926eac66163ad33570c1e1f717a89de21e5
Ezgi Keserci <keserci.ezgi@gmail.com>
2024-01-22 22:57:41 +0300

    Add project files.

---
+++ Assets/Scripts/Book.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Book
+{
+    public string bookISBN;
+    public string bookTitle;
+    public string bookAuthor;
+    public string publisher;
+
+}
